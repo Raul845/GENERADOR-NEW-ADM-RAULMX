6a4f7b8f41abd2adb4dedff70cdf2a312c255a0d
Raul845 <58236956+Raul845@users.noreply.github.com>
2019-12-06 17:45:34 -0600

    Add files via upload

--- request/gerar.sh
+++ request/gerar.sh
@@ -4,10 +4,9 @@ clear
 [[ -e /etc/newadm-instalacao ]] && BASICINST="$(cat /etc/newadm-instalacao)" || BASICINST="menu PGet.py ports.sh ADMbot.sh message.txt usercodes sockspy.sh POpen.py PPriv.py PPub.py PDirect.py speedtest.py speed.sh utils.sh dropbear.sh apacheon.sh openvpn.sh shadowsocks.sh ssl.sh sslmanager.sh squid.sh v2ray.sh vdoray.sh errormanager.sh shadowsock.sh shadown.sh ssrrmu.sh hora.sh htop.sh panelweb.sh Crear-Demo.sh nload.sh insta_painel painel.zip dados.zip"
 IVAR="/etc/http-instas"
 system=$(cat /etc/issue.net)
-BARRA="\033[1;33m<=>=<=>=<=>=<=>=<=>=<=>=<=>=<=>=<=>=<=>=<=>=<=>\033[0m"
+BARRA="\033[1;33m<=>=<=>=<=>=<=>=<=>=<=>=<=>=<=>=<=>=<=>=<=>=<=>\033[1;33m"
 echo -e "$BARRA"
 cat << EOF
-<=>=<=>=<=>=<=>=<=>=<=>=<=>=<=>=<=>=<=>=<=>=<=>
                   GENERADOR               
            [ NEW-ULTIMATE-ADM-PRO ]
                RAUL_FERRAL_MX
