de3f7f3d9aed9b599c4174ef64950202a845d682
Raul845 <58236956+Raul845@users.noreply.github.com>
2019-12-07 12:59:21 -0600

    Add files via upload

--- request/htop.sh
+++ request/htop.sh
@@ -3,7 +3,7 @@ Block="/etc/bin" && [[ ! -d ${Block} ]] && exit
 Block > /dev/null 2>&1
 clear
 cowsay -f eyes "con esta herramienta puedes verificar los procesos del sistema...." | lolcat 
-figlet RAUL_MX | lolcat
+figlet ..RAUL_MX.. | lolcat
 echo -e "\e[0;31m<=>=<=>=<=>=<=>=<=>=<=>=<=>=<=>=<=>=<=>=<=>=<=>\e[0m"
 echo -e "\033[47;31mATENCION: \033[1;34m LA INSTALACION PUEDE DEMORAR UNOS MINUTOS\033[1;31m\033[0m"
 echo -e "\e[0;31m<=>=<=>=<=>=<=>=<=>=<=>=<=>=<=>=<=>=<=>=<=>=<=>\e[0m"
