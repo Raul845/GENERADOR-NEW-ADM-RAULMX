e23ed6a3bf7caeb8cf7ca63e49e9d5e6701283d7
Raul845 <58236956+Raul845@users.noreply.github.com>
2019-12-09 23:54:20 -0600

    Add files via upload

--- request/errormanager.sh
+++ request/errormanager.sh
@@ -10,7 +10,7 @@ SCPinst="/etc/ger-inst"
 SCPidioma="${SCPdir}/idioma"
 
 declare -A cor=( [0]="\033[1;37m" [1]="\033[1;34m" [2]="\033[1;35m" [3]="\033[1;32m" [4]="\033[1;31m" [5]="\033[1;33m" [6]="\E[44;1;37m" [7]="\E[41;1;37m" )
-barra="\e[1;33m<=>=<=>=<=>=<=>=<=>=<=>=<=>=<=>=<=>=<=>=<=>=<=>=<=>=<=>\e[1;33m"
+barra="\e[1;33m<=>=<=>=<=>=<=>=<=>=<=>=<=>=<=>=<=>=<=>=<=>=<=>\e[1;33m"
 SCPdir="/etc/newadm" && [[ ! -d ${SCPdir} ]] && exit 1
 SCPfrm="/etc/ger-frm" && [[ ! -d ${SCPfrm} ]] && exit
 SCPinst="/etc/ger-inst" && [[ ! -d ${SCPinst} ]] && exit
@@ -80,13 +80,9 @@ msg -ama " $(fun_trans "LOS PUERTOS SSL SEAN DETENIDO CON EXITO")"
 msg -bar
 }
 corregir_fun () {
-echo -e " 
-figlet -f small  CORREGIDOR
-echo -e "\E[41;1;37m $(fun_trans      [ CORREGIDOR DE PUERTOS,DROPBEAR,EQUID Y SSL ] \E[41;1;37m" 
-echo -e "
-msg -bar2
-echo -e"\E[41;1;37m         [ ADM-PRO-RAUL-FERRAL-MX ]         \E[41;1;37m"
-echo " $barra"
+echo -e " \E[41;1;33m        [ CORREGIDOR DE PUERTOS DROPBEAR,SQUID,SSL ]      \E[41;1;33m"
+echo -e " \E[41;1;33m                     [ NEW-ADM-PRO-RAUL-FERRAL-MX ]                       \E[41;1;33m"
+echo -e "$barra"
 while true; do
 echo -e "${cor[4]} [1] > ${cor[5]}$(fun_trans "CORREGIR ERROR DE DROPBEAR")"
 echo -e "${cor[4]} [2] > ${cor[5]}$(fun_trans "CORREGIR ERROR SQUID")"
