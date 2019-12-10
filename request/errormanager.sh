5304d1d2f9b6659df11d8ccc352ab735cea7016f
Raul845 <58236956+Raul845@users.noreply.github.com>
2019-12-09 22:56:49 -0600

    Add files via upload

--- request/errormanager.sh
+++ request/errormanager.sh
@@ -10,7 +10,7 @@ SCPinst="/etc/ger-inst"
 SCPidioma="${SCPdir}/idioma"
 
 declare -A cor=( [0]="\033[1;37m" [1]="\033[1;34m" [2]="\033[1;35m" [3]="\033[1;32m" [4]="\033[1;31m" [5]="\033[1;33m" [6]="\E[44;1;37m" [7]="\E[41;1;37m" )
-barra="\033[0m\e[31m======================================================\033[1;37m"
+barra="\e[1;33m<=>=<=>=<=>=<=>=<=>=<=>=<=>=<=>=<=>=<=>=<=>=<=>=<=>=<=>=<=><=>\e[1;33m"
 SCPdir="/etc/newadm" && [[ ! -d ${SCPdir} ]] && exit 1
 SCPfrm="/etc/ger-frm" && [[ ! -d ${SCPfrm} ]] && exit
 SCPinst="/etc/ger-inst" && [[ ! -d ${SCPinst} ]] && exit
@@ -82,12 +82,10 @@ msg -bar
 corregir_fun () {
 echo -e " "
 msg -bar2
-msg -bra "\E[41;1;37m   [ CORREGIDOR DE PUERTOS,DROPBEAR,EQUID Y SSL ]   \E[41;1;37m" 
+msg -bra "\E[41;1;37m  [ CORREGIDOR DE PUERTOS,DROPBEAR,EQUID Y SSL ] \E[41;1;37m" 
 echo -e " "
 msg -bar2
-msg -bra "\E[41;1;37m       [ ADM-PRO-RAUL-FERRAL-MX ]       \E[41;1;37m"
-echo -e " "
-msg -bar2
+msg -bra "\E[41;1;37m         [ ADM-PRO-RAUL-FERRAL-MX ]         \E[41;1;37m"
 echo " $barra"
 while true; do
 echo -e "${cor[4]} [1] > ${cor[5]}$(fun_trans "CORREGIR ERROR DE DROPBEAR")"
