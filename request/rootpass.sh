639c745436b7f2158d6045af3387c00022c87176
Raul845 <58236956+Raul845@users.noreply.github.com>
2019-12-11 17:18:16 -0600

    Add files via upload

--- request/rootpass.sh
+++ request/rootpass.sh
@@ -26,22 +26,23 @@ done
 echo -e " \033[1;33m[\033[1;31m####################\033[1;33m] - \033[1;32m100%\033[0m"
 sleep 1s
 }
-cowsay -f eyes "esta herramienta cambia a usuario ROOT las vps de googlecloud y amazon...." | lolcat 
-figlet ..dankelthaher.. | lolcat
+
+figlet -f small NEW=ADM=PRO | lolcat
 sleep 4s
 sed -i "s;PermitRootLogin prohibit-password;PermitRootLogin yes;g" /etc/ssh/sshd_config
 sed -i "s;PermitRootLogin without-password;PermitRootLogin yes;g" /etc/ssh/sshd_config
 sed -i "s;PasswordAuthentication no;PasswordAuthentication yes;g" /etc/ssh/sshd_config
-echo -e "\033[47;31mATENCION: \033[1;34m ESTA CONTRASENA SERA USADA PARA ENTRAR A SU SERVIDOR LOGEADO COMO ROOT\033[1;31m\033[0m"
-echo -e "\e[0;31m➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖\e[0m"
+echo -e "\033[47;31mATENCION: \033[1;34m ESTA CONTRASENA SERA USADA PARA ENTRAR A SU SERVIDOR
+\033[1;31m\033[0m"
+echo -e "\e[1;33m<=>=<=>=<=>=<=>=<=>=<=>=<=>=<=>=<=>=<=>=<=>=<=>\e[1;33m"
 echo -e "\033[1;31mESCRIBA SU NUEVA CONTRASENA\e[0m"
-echo -e "\e[0;31m➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖\e[0m"
+echo -e "\e[1;33m<=>=<=>=<=>=<=>=<=>=<=>=<=>=<=>=<=>=<=>=<=>=<=>\e[1;33m"
 read  -p ": " pass
 (echo $pass; echo $pass)|passwd 2>/dev/null
 sleep 1s
-echo -e "\e[0;31m➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖\e[0m"
+echo -e "\e[1;33m<=>=<=>=<=>=<=>=<=>=<=>=<=>=<=>=<=>=<=>=<=>=<=>\e[1;33m"
 echo -e "\033[1;34mCONTRASENA AGREGADA CON EXITO!\e[0m"
-echo -e "\e[0;31m➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖\e[0m"
+echo -e "\e[1;33m<=>=<=>=<=>=<=>=<=>=<=>=<=>=<=>=<=>=<=>=<=>=<=>\e[1;33m"
 echo -e "\033[1;34mSU CONTRASENA NUEVA ES: \033[1;32m$pass\e[0m"
 
 echo ""
