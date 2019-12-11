1884256a8a777f8c69f86de07bb306fc26df2596
Raul845 <58236956+Raul845@users.noreply.github.com>
2019-12-11 14:08:37 -0600

    Add files via upload

--- request/Crear-Demo.sh
+++ request/Crear-Demo.sh
@@ -19,7 +19,7 @@ SEMCOR='\e[0m'
   -azu)cor="${MAG}${NEGRITO}" && echo -e "${cor}${2}${SEMCOR}";;
   -verd)cor="${VERDE}${NEGRITO}" && echo -e "${cor}${2}${SEMCOR}";;
   -bra)cor="${BRAN}${NEGRITO}" && echo -e "${cor}${2}${SEMCOR}";;
-  "-bar2"|"-bar")cor="${AZUL}<=>=<=>=<=>=<=>=<=>=<=>=<=>=<=>=<=>=<=>=<=>=<=>=<=>=<=>=<=><=>\e[0m" && echo -e "${SEMCOR}${cor}${SEMCOR}";;
+  "-bar2"|"-bar")cor="${AZUL}======================================================" && echo -e "${SEMCOR}${cor}${SEMCOR}";;
  esac
 }
 tmpusr () {
