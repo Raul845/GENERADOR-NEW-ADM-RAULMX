f51c74fe29c1868f7b79c9128d13b54e314721cd
Raul845 <58236956+Raul845@users.noreply.github.com>
2020-03-16 00:08:28 -0500

    Add files via upload

--- request/gerar.sh
+++ request/gerar.sh
@@ -6,12 +6,12 @@ IVAR="/etc/http-instas"
 system=$(cat /etc/issue.net)
 BARRA="\033[1;33m×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×\033[1;33m"
 echo -e "$BARRA"
-figlet ×VPS-PRO× | lolcat
-EOF
-×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×           
-              Version : ORIGINAL
+cat << EOF
 ×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×
-              INSTALACIONES: $(cat $IVAR)
+           NEW KEY GENERADOR BY RAUl_FERRAl_MX
+              VERCION : ORIGINAL
+×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×
+               INSTALACIONES: $(cat $IVAR)
 ×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×
 EOF
 SCPT_DIR="/etc/SCRIPT"
@@ -283,6 +283,7 @@ unset PID_GEN
 PID_GEN=$(ps x|grep -v grep|grep "http-server.sh")
 [[ ! $PID_GEN ]] && PID_GEN="\033[1;31mAPAGADO" || PID_GEN="\033[1;34mEN LINEA"
 echo -e "\E[41;1;37m               CREDITOS:RAUL_FERRAL_MX Y ALEXMOD80\E[41;1;37m"
+echo -e " "
 echo -e "$BARRA"
 echo -e "DIRECTORIO \033[1;31m${SCPT_DIR}\033[0m"
 echo -e "\e[1;34mSISTEMA : \e[1;31m$system"
