4efe2c3c4f6eeefdfb5434a954f3a2150eff5f38
Raul845 <58236956+Raul845@users.noreply.github.com>
2020-03-15 23:38:00 -0500

    Add files via upload

--- request/gerar.sh
+++ request/gerar.sh
@@ -278,9 +278,9 @@ meu_ip
 unset PID_GEN
 PID_GEN=$(ps x|grep -v grep|grep "http-server.sh")
 [[ ! $PID_GEN ]] && PID_GEN="\033[1;31mAPAGADO" || PID_GEN="\033[1;34mEN LINEA"
-echo -e "\E[41;1;37m           CREDITOS:\E[41;1;37m"
-echo -e "\E[41;1;37mRAUL_FERRAL_MX \E[41;1;37m"
-echo -e "\E[41;1;37mALEXMOD80\E[41;1;37m"
+msg -bar2
+msg -bra "\E[41;1;37m               CREDITOS:RAUL_FERRAL_MX Y ALEXMOD80\E[41;1;37m"
+echo -e " "
 echo -e "$BARRA"
 echo -e "DIRECTORIO \033[1;31m${SCPT_DIR}\033[0m"
 echo -e "\e[1;34mSISTEMA : \e[1;31m$system"
